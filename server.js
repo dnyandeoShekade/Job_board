00e6fb6a2d66b853ca42341622b1f2592bf5e43c
dnyandeo-max <dnyandeo@innovins.com>
2026-08-10 00:50:29 +0530

    ..

--- server.js
+++ server.js
@@ -74,11 +74,12 @@ connectDB().catch((err) => {
 });
 
 // For local development
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+
 
 module.exports = app;
